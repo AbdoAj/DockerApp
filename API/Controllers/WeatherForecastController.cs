38bb3a956f553f8561f80f72e6d472e3f6d0f57e
Abdo Alrujbani <abdoo.aj93@gmail.com>
2025-10-19 17:35:47 +0200

    updates

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Dokcer Update v1.2"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
